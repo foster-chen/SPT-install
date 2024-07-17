5d3c60d64a005e5cc9f88b942b212bb501e12c9f
Foster Chen <fosterchen1998@gmail.com>
2024-07-18 00:46:40 +0800

    [WIP] 1. added mod name check 2. added download link and version crawl

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 import argparse
-
+import json
+import os
+from tqdm import tqdm
+from datetime import datetime
 
 COLOR_DICT = {
         'red': '\033[91m',
@@ -15,7 +18,8 @@ def parse_args(args):
     parser = argparse.ArgumentParser(prog="python3 main.py",
                                      description="install mods from hubMods.txt")
     parser.add_argument('spt_path', help="installation path of SPT")
-
+    parser.add_argument('--include_outdated', action="store_true", help="sync mods even if it is outdated")
+    parser.add_argument('--use_cache', action="store_true", help="use webpage information from cache file")
     args = parser.parse_args(args)
 
     return args
@@ -41,10 +45,71 @@ def colorize(string, color):
     assert color in COLOR_DICT
     return f"{COLOR_DICT[color]}{string}{COLOR_DICT['reset']}"
 
+def fetch_mod_tabs(URL, args, pages=40):
+    if args.use_cache and os.path.exists("webCache.json"):
+        with open("webCache.json", "r") as rf:
+            data = json.load(rf)
+            tabs = data["tabs"]
+            print(f'{colorize("WARNING:", "red")} using cache file create on {data["date"]}')
+        for key in tabs:
+            tabs[key] = BeautifulSoup(tabs[key], "html.parser")
+    else:
+        tabs = {}
+        for i in tqdm(range(pages), desc="Fetching from SPT hub: "):
+            currURL = URL.format(i)
+            response = requests.get(currURL)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            items = soup.find('ol', class_='filebaseFileList')
+            currTabs = items.find_all('div', class_="filebaseFileDataContainer")
+            for tab in tqdm(currTabs, leave=False):
+                modName = tab.find("h3", class_="filebaseFileSubject").text.strip()
+                tabs[modName] = tab.parent.parent
+                
+        with open("webCache.json", "w") as wf:
+            result = {"date": datetime.now().strftime("%Y-%m-%d"),
+                      "tabs": {n: tabs[modName].prettify() for n in tabs}}
+            json.dump(result, wf)
+    return tabs
+
+
 def main(args):
     args = parse_args(args)
+    with open('config.json', 'r') as rf:
+        config = json.load(rf)
+    with open("hubMods.txt", "r") as rf:
+        hubMods = [l.strip() for l in rf.readlines()]
+    modTabs = fetch_mod_tabs(config["url"], args=args)
 
+    # check mod names
+    hasDiff = False
+    for i, modName in tqdm(enumerate(hubMods), desc="Verifying hub mod list: ", total=len(hubMods)):
+        highestScore = 0
+        hubName = None
+        for modHubName in modTabs:
+            score = dice_coefficient(modName, modHubName)
+            if score >= highestScore:
+                highestScore = score
+                hubName = modHubName
+        # name is different from the hub, make corrections
+        if hubName != modName:
+            print(f"\"{colorize(modName, 'red')}\" has been changed to \"{colorize(hubName, 'red')}\"")
+            hubMods[i] = hubName
+            hasDiff = True
+        # create config entry for new mod
+        if not config["hubMods"].get(hubName) or not args.use_cache:
+            modUrl = modTabs[hubName].find("a").get("href")
+            modPage = BeautifulSoup(requests.get(modUrl).content, "html.parser")
+            downloadLink = modPage.find("nav", class_="contentHeaderNavigation").find("a").get("href")
+            config["hubMods"][hubName] = {"version": modTabs[hubName].find("ul", class_="labelList").text.strip(),
+                                          "download": BeautifulSoup(requests.get(downloadLink).content, "html.parser").find("a").get("href")}
+            hasDiff = True
+    if hasDiff:    
+        with open("hubMods.txt", "w") as wf:
+            for line in hubMods:
+                wf.write(f"{line}\n")
+        with open("config.json", "w") as wf:
+            json.dump(config, wf)
+    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
