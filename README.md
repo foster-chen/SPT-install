f8ddc77b345c0b3af0fab4dfdb42a49b50c57614
Foster Chen <fosterchen1998@gmail.com>
2024-07-18 22:23:09 +0800

    [Ver. 1.0]

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # SPT-install
-Foster's personal Single Player Tarkov update helper
+My personal Single Player Tarkov installation and mod update helper for friends.
+
+## 1. Install SPT
+
+Update to the latest EFT, then download the SPT installer [here](https://sp-tarkov.com/#download). Place the insatller into a new folder **OUTSIDE** the vanilla EFT folder. Run the installer.
+
+## 2. Setup Python
+
+Install Python if not already done so from [here](https://www.python.org/downloads/windows/). Check your installation by typing `python3 -V` or `python -V` in the cli. A version newer than `3.9` is recommended.
+
+```bash
+$ python3 -V
+
+Python 3.12.4
+```
+
+## 3. Install Python dependencies
+
+Go to the cloned downloaded repository, run the following command to install dependant Python libraries.
+
+```bash
+$ python3 -m pip install -r ./requirements.txt
+```
+
+## 4. Run the program to check for mods in the mod list `hubMods.txt`
+
+You can obtain the website cache `webcache.json` from me and place it in the repository, then run `python3 main.py ${my/SPT/path} --use_cache`. Replace `${my/SPT/path}` with the actual installation path of your SPT. You could also run without the `--use_cache` flag, crawl the hub and generate the `webcache.json` yourself and get the latest mod updates and download links, which might take longer.
+
+## Adding more mods to the wishlist
+
+if you wish to add more mods to the list, simply add the name of the mod to the end of hub_mods.txt. Don't worry on getting the name exactly right as the program can search the name and replace it for you!
--- main.py
+++ main.py
@@ -6,23 +6,37 @@ import json
 import os
 from tqdm import tqdm
 from datetime import datetime
+import gdown
+
 
 COLOR_DICT = {
         'red': '\033[91m',
         'green': '\033[92m',
+        'yellow': '\033[93m',
+        'blue': '\033[94m',
         'reset': '\033[0m'
         }
 
-
 def parse_args(args):
     parser = argparse.ArgumentParser(prog="python3 main.py",
-                                     description="install mods from hubMods.txt")
+                                     description="install mods from hub_mods.txt")
     parser.add_argument('spt_path', help="installation path of SPT")
     parser.add_argument('--include_outdated', action="store_true", help="sync mods even if it is outdated")
     parser.add_argument('--use_cache', action="store_true", help="use webpage information from cache file")
     args = parser.parse_args(args)
 
     return args
+
+def fetch(url, timeout=10):
+    while True:
+        try:
+            response = requests.get(url, timeout=timeout)
+            return response
+        except requests.exceptions.ConnectionError:
+            ans = input(f"ConnectionError with {url}. Retry? ([Y]/N)")
+            if ans == "N":
+                raise requests.exceptions.ConnectionError
+
 
 def dice_coefficient(s1, s2):
     # Convert strings to lowercase and remove spaces
@@ -46,18 +60,19 @@ def colorize(string, color):
     return f"{COLOR_DICT[color]}{string}{COLOR_DICT['reset']}"
 
 def fetch_mod_tabs(URL, args, pages=40):
-    if args.use_cache and os.path.exists("webCache.json"):
-        with open("webCache.json", "r") as rf:
+    if args.use_cache and os.path.exists("webcache.json"):
+        with open("webcache.json", "r") as rf:
             data = json.load(rf)
             tabs = data["tabs"]
-            print(f'{colorize("WARNING:", "red")} using cache file create on {data["date"]}')
+            message =  f"using cache file created on {data['date']}"
+            print(f"{colorize(message, 'yellow')}")
         for key in tabs:
             tabs[key] = BeautifulSoup(tabs[key], "html.parser")
     else:
         tabs = {}
         for i in tqdm(range(pages), desc="Fetching from SPT hub: "):
             currURL = URL.format(i)
-            response = requests.get(currURL)
+            response = fetch(currURL, timeout=10)
             soup = BeautifulSoup(response.content, 'html.parser')
             items = soup.find('ol', class_='filebaseFileList')
             currTabs = items.find_all('div', class_="filebaseFileDataContainer")
@@ -65,20 +80,49 @@ def fetch_mod_tabs(URL, args, pages=40):
                 modName = tab.find("h3", class_="filebaseFileSubject").text.strip()
                 tabs[modName] = tab.parent.parent
                 
-        with open("webCache.json", "w") as wf:
+        with open("webcache.json", "w") as wf:
             result = {"date": datetime.now().strftime("%Y-%m-%d"),
                       "tabs": {n: tabs[modName].prettify() for n in tabs}}
             json.dump(result, wf)
     return tabs
 
+def is_up_to_date(version, targetVersion):
+    def numeric_version_compare(version: list, targetVersion: list):
+        return all(x >= y for x, y in zip(version, targetVersion)) 
+    assert targetVersion.startswith("SPT "), "targetSptVersion must start with \"SPT\""
+    targetVersion = [int(i) for i in targetVersion.replace(".x", ".0").split()[-1].split(".")]
+    while len(targetVersion) < 3:
+        targetVersion.append(0)
+    version = version.split("-")[-1].replace(".X", ".0")
+    version = [int(i) for i in version.split()[-1].split(".")]
+    return numeric_version_compare(version, targetVersion)
+
+def is_installed_up_to_date(installation, targetVersion, args):
+    if not installation:
+        return False
+    for mod in installation["BepInEx"]:
+        if not os.path.exists(os.path.join(args.spt_path, "BepInEx/plugins/", mod)):
+            return False
+
+    for mod in installation["user"]:
+        if os.path.exists(os.path.join(args.spt_path, "user/mods/", mod)):
+            packageJson = os.path.join(args.spt_path, "user/mods/", mod, "package.json")
+            with open(packageJson, 'r') as rf:
+                f = json.load(rf)
+                if not is_up_to_date(f["sptVersion"], targetVersion):
+                    return False
+        else:
+            return False
+    return True
 
 def main(args):
     args = parse_args(args)
-    with open('config.json', 'r') as rf:
+    with open('manifest.json', 'r') as rf:
         config = json.load(rf)
-    with open("hubMods.txt", "r") as rf:
+    with open("hub_mods.txt", "r") as rf:
         hubMods = [l.strip() for l in rf.readlines()]
     modTabs = fetch_mod_tabs(config["url"], args=args)
+    targetSptVersion = config["targetSptVersion"]
 
     # check mod names
     hasDiff = False
@@ -92,24 +136,52 @@ def main(args):
                 hubName = modHubName
         # name is different from the hub, make corrections
         if hubName != modName:
-            print(f"\"{colorize(modName, 'red')}\" has been changed to \"{colorize(hubName, 'red')}\"")
+            print(f"\"{colorize(modName, 'yellow')}\" has been changed to \"{colorize(hubName, 'yellow')}\"")
             hubMods[i] = hubName
             hasDiff = True
-        # create config entry for new mod
+        # create config entry for new mod or update with new info
         if not config["hubMods"].get(hubName) or not args.use_cache:
             modUrl = modTabs[hubName].find("a").get("href")
-            modPage = BeautifulSoup(requests.get(modUrl).content, "html.parser")
+            modPage = BeautifulSoup(fetch(modUrl, timeout=10).content, "html.parser")
             downloadLink = modPage.find("nav", class_="contentHeaderNavigation").find("a").get("href")
             config["hubMods"][hubName] = {"version": modTabs[hubName].find("ul", class_="labelList").text.strip(),
-                                          "download": BeautifulSoup(requests.get(downloadLink).content, "html.parser").find("a").get("href")}
+                                          "download": BeautifulSoup(fetch(downloadLink, timeout=20).content, "html.parser").find("a").get("href")}
             hasDiff = True
-    if hasDiff:    
-        with open("hubMods.txt", "w") as wf:
+    if hasDiff:
+        with open("hub_mods.txt", "w") as wf:
             for line in hubMods:
                 wf.write(f"{line}\n")
-        with open("config.json", "w") as wf:
+        with open("manifest.json", "w") as wf:
             json.dump(config, wf)
     
+    # check installation/download mods
+    download_list = []
+    skipped_list = []
+    for modName, modInfo in config["hubMods"].items():
+        # skipping mods with outdated dependencies
+        if modInfo.get("dependencies"):
+            for dependedMod in modInfo["dependencies"]:
+                if not is_up_to_date(config["hubMods"][dependedMod]["version"], targetSptVersion) and not args.include_outdated:
+                    print(f"{colorize('WARNING:', 'red')} skipping \"{modName}\" because it depends on an outdated mod \"{dependedMod}\" ({colorize(config["hubMods"][dependedMod]["version"], 'red')})")
+                    skipped_list.append(modName)
+                    break
+        # skipping outdated mods
+        elif not is_up_to_date(modInfo["version"], targetSptVersion) and not args.include_outdated:
+            print(f"{colorize('WARNING:', 'red')} skipping \"{modName}\" because it is outdated ({colorize(modInfo['version'], 'red')})")
+            skipped_list.append(modName)
+        else:
+            if is_installed_up_to_date(modInfo.get("installation"), targetSptVersion, args):
+                print(f"{colorize('-> ', 'blue')}\"{modName}\" is installed and {colorize('up-to-date', 'green')}")
+                continue
+            color = "green" if is_up_to_date(modInfo["version"], targetSptVersion) else "red"
+            print(f"{colorize('-> ', 'blue')}{modName} ({colorize(modInfo['version'], color)}): {colorize(modInfo['download'], 'yellow')}")
+            download_list.append(modName)
+            input("Press Enter to continue...")
+    
+    download_list_message = '\n'.join([colorize(f"\t{modName} ({config["hubMods"][modName]["version"]})", 'green') for modName in download_list])
+    skipped_list_message = '\n'.join([colorize(f"\t{modName} ({config["hubMods"][modName]["version"]})", 'red') for modName in skipped_list])
+    print(f"\n{colorize('-> ', 'blue')}Downloadable mods:\n{download_list_message}")
+    print(f"\n{colorize('-> ', 'blue')}Skipped mods:\n{skipped_list_message}")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
